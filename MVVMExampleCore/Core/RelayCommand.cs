b190ef3941673e6afbbe0db700fa83d5988cd898
teacher210 <teacher210@yandex.ru>
2021-10-26 12:40:56 +0400

    Add project files.

---
+++ MVVMExampleCore/Core/RelayCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVMExampleCore.Core
+{
+    class RelayCommand : ICommand
+    {
+        private readonly Func<object, bool> _canExecute;
+        private readonly Action<object> _execute;
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this._canExecute = canExecute;
+            this._execute = execute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this._canExecute == null || this._canExecute(parameter); ;
+        }
+
+        public void Execute(object parameter)
+        {
+            this._execute(parameter);
+        }
+    }
+}
---
+++ MVVMExampleCore/ViewModel/ViewModel.cs
@@ -0,0 +1,38 @@
+﻿using MVVMExampleCore.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVMExampleCore.ViewModel
+{
+    class ViewModel : ObserverObject
+    {
+
+        private int _ClickCoint;
+        public int ClickCount
+        {
+            get { return _ClickCoint; }
+            set {
+                _ClickCoint = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool IsClick(object parameter)
+        {
+            return ClickCount < 10;
+        }
+
+        private void ClickEvent(object parameter)
+        {
+            ClickCount++;
+        }
+
+        public RelayCommand ClickCommand => new RelayCommand(ClickEvent, IsClick);
+    }
+}
